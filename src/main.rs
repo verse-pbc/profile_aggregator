b71d5216049420a810a2b00aa7357ff08aae876e
Daniel Cadenas <dcadenas@gmail.com>
2025-07-04 13:01:48 -0300

    Less verbose logs

--- src/main.rs
+++ src/main.rs
@@ -408,7 +408,7 @@ async fn main() -> Result<()> {
     } else {
         // Setup normal logging if not using tokio-console
         let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-            EnvFilter::new("info,profile_aggregator=debug,nostr_relay_builder=debug")
+            EnvFilter::new("info,profile_aggregator=debug,nostr_relay_builder=debug,nostr_relay_pool::relay::inner=off")
         });
         fmt().with_env_filter(env_filter).with_target(true).init();
     }
